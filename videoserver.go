ae43324ebccd219fce61614c7253e71668c5b5c9
zac <zacharyjacobcollins@gmail.com>
2016-09-18 00:37:51 +0000

    time updates

--- videoserver.go
+++ videoserver.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+    "fmt"
     "net/http"
+    "time"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("../../../videos"))
+	currentTime := time.Now().UTC.Format("2006-05-05 15:00:00")
+
+	fmt.Println(currentTime);
+
+	//Start the webserver
+	fs := http.FileServer(http.Dir("../../../../../videos"))
 	http.Handle("/", http.StripPrefix("/", fs))
 	http.ListenAndServe(":8080", nil)
 }
