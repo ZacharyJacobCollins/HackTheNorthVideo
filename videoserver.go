0167f0c2433204717a495e6c98a41b385ac6d76f
Zac Collins <Zac Collins>
2016-09-17 23:58:33 -0400

    updates

--- videoserver.go
+++ videoserver.go
@@ -4,24 +4,57 @@ import (
     "net/http"
     "time"
     "io/ioutil"
+    "fmt"
+    "os/exec"
 )
 
 func main() {
-	//Get the current utc timestamp
-	now := time.Now().UTC().Format("20060102150405")
+    scanCams()
 
-	//write time to text file 
-	timestamp := []byte(now)
-	err := ioutil.WriteFile("./timestamp.txt", timestamp, 0644)
-	if (err != nil) {
-		panic(err)
-	}
+}
 
+/**
+*   Retrieves available webcams attached to network.
+*   @return [array of cam names as strings]
+*/
+func scanCams() {
+  cmd := exec.Command("ls", "/dev/video*")
+  out, err := cmd.Output()
+  check(err)
+  fmt.Print(string(out))
+}
 
-	
-	//Start the webserver
+/**
+*   Retrieves available audio devices attached to network.
+*   @return [array of audio devices as strings]
+*/
+func scanAudio() {
+
+}
+
+//Takes the name of the webcam and starts recording inside of a go routine.
+func startWebcam() {
+
+}
+
+func startWebserver() {
+  //Start the webserver
 	fs := http.FileServer(http.Dir("../../../../../videos"))
 	http.Handle("/", http.StripPrefix("/", fs))
 	http.ListenAndServe(":8080", nil)
 }
 
+//Get the current utc timestamp, write time to text file
+func heartBeat() {
+  now := time.Now().UTC().Format("20060102150405")
+  timestamp := []byte(now)
+  err := ioutil.WriteFile("./timestamp.txt", timestamp, 0644)
+  check(err)
+}
+
+//Print error if err
+func check(e error) {
+	if(e != nil) {
+		fmt.Println(e.Error())
+	}
+}
